98d3be1eb34fcde5ce1188b47d21b442911c125f
Muthukumar s <muthu.triadtechsolutions@gmail.com>
2025-08-08 00:03:41 +0530

    Menu twig, js and css updated

--- web/themes/custom/tts/js/global.js
+++ web/themes/custom/tts/js/global.js
@@ -10,6 +10,18 @@
   Drupal.behaviors.tts = {
     attach: function (context, settings) {
 
+      jQuery(document).ready(function ($) {
+        const $menu = $('#CollapsingNavbar');
+        const $closeBtn = $('.close-menu-btn');
+
+        // Close menu
+        $closeBtn.on('click', function () {
+          if ($(window).width() <= 1024) {
+            $menu.removeClass('show');
+          }
+        });
+      });
+
     }
   };
 
